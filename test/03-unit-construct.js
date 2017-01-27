6f348abde4772a2c5ca21704bfbd1cf050a3831b
nomilous <nomilous@gmail.com>
2017-01-27 17:01:38 +0200

    exchange calls operations.run

---
+++ test/03-unit-construct.js
@@ -0,0 +1,179 @@
+var expect = require('expect.js');
+
+var HappnerClient = require('..');
+var OperationsProvider = require('../lib/providers/operations-provider');
+var ConnectionProvider = require('../lib/providers/connection-provider');
+
+describe('03 - unit - construct', function () {
+
+  beforeEach(function () {
+    this.originalRun = OperationsProvider.prototype.run;
+  });
+
+  afterEach(function () {
+    OperationsProvider.prototype.run = this.originalRun;
+  });
+
+  context('exchange', function () {
+
+    it('errors on model without version declared', function (done) {
+      var model = {
+        component1: {
+          // version: '^1.0.0',
+          methods: {
+            method1: {},
+            method2: {}
+          }
+        }
+      };
+
+      var c = new HappnerClient();
+
+      try {
+        var api = c.construct(model);
+      } catch (e) {
+        expect(e.message).to.be('Missing version');
+        done();
+      }
+    });
+
+    it('errors on model without methods declared', function (done) {
+      var model = {
+        component1: {
+          version: '^1.0.0',
+          // methods: {
+          //   method1: {},
+          //   method2: {}
+          // }
+        }
+      };
+
+      var c = new HappnerClient();
+
+      try {
+        var api = c.construct(model);
+      } catch (e) {
+        expect(e.message).to.be('Missing methods');
+        done();
+      }
+    });
+
+    it('builds exchange functions from model', function () {
+      var model = {
+        component1: {
+          version: '^1.0.0',
+          methods: {
+            method1: {},
+            method2: {}
+          }
+        },
+        component2: {
+          version: '^1.0.0',
+          methods: {
+            method1: {},
+            method2: {}
+          }
+        }
+      };
+
+      var c = new HappnerClient();
+
+      var api = c.construct(model);
+
+      expect(api.exchange.component1.method1).to.be.a(Function);
+      expect(api.exchange.component1.method2).to.be.a(Function);
+      expect(api.exchange.component2.method1).to.be.a(Function);
+      expect(api.exchange.component2.method2).to.be.a(Function);
+    });
+
+    it('calls operations provider on calls to exchange functions', function (done) {
+      OperationsProvider.prototype.run = function (component, version, method) {
+        expect(component).to.be('component1');
+        expect(version).to.be('^1.0.0');
+        expect(method).to.be('method1');
+        done();
+      };
+
+      var model = {
+        component1: {
+          version: '^1.0.0',
+          methods: {
+            method1: {}
+          }
+        }
+      };
+
+      var c = new HappnerClient();
+      var api = c.construct(model);
+
+      api.exchange.component1.method1();
+    });
+
+    it('supports promises on exchange calls', function (done) {
+      OperationsProvider.prototype.run = function (component, version, method, args, callback) {
+        callback(null, {RE:'SULT'})
+      };
+
+      var model = {
+        component1: {
+          version: '^1.0.0',
+          methods: {
+            method1: {}
+          }
+        }
+      };
+
+      var c = new HappnerClient();
+      var api = c.construct(model);
+
+      api.exchange.component1.method1('ARG1')
+        .then(function (result) {
+          expect(result).to.eql({RE:'SULT'});
+        })
+        .then(done).catch(done);
+    });
+
+    it('supports callbacks on exchange calls', function (done) {
+      OperationsProvider.prototype.run = function (component, version, method, args, callback) {
+        callback(null, {RE:'SULT'});
+      };
+
+      var model = {
+        component1: {
+          version: '^1.0.0',
+          methods: {
+            method1: {}
+          }
+        }
+      };
+
+      var c = new HappnerClient();
+      var api = c.construct(model);
+
+      api.exchange.component1.method1('ARG1', function(e, result) {
+        if (e) return done(e);
+        expect(result).to.eql({RE:'SULT'});
+        done();
+      });
+    });
+
+  });
+
+  context('event', function () {
+
+  });
+
+  context('data', function () {
+
+  });
+
+  context('model validation', function () {
+
+    it('checks the model against existing description');
+
+    it('checks the model against pending description');
+
+  });
+
+})
+;
